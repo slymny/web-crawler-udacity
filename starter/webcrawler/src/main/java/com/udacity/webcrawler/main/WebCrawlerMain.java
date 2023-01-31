8e40e0061d409ae7d24bd52c932f604d8d4c7239
Suleyman Yilmaz <yilmazsgit@gmail.com>
2023-01-31 18:58:11 +0100

    bug fixes

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -11,6 +11,7 @@ import com.udacity.webcrawler.profiler.Profiler;
 import com.udacity.webcrawler.profiler.ProfilerModule;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Path;
@@ -18,52 +19,55 @@ import java.util.Objects;
 
 public final class WebCrawlerMain {
 
-  private final CrawlerConfiguration config;
+    private final CrawlerConfiguration config;
+    @Inject
+    private WebCrawler crawler;
+    @Inject
+    private Profiler profiler;
 
-  private WebCrawlerMain(CrawlerConfiguration config) {
-    this.config = Objects.requireNonNull(config);
-  }
-
-  @Inject
-  private WebCrawler crawler;
-
-  @Inject
-  private Profiler profiler;
-
-  private void run() throws Exception {
-    Guice.createInjector(new WebCrawlerModule(config), new ProfilerModule()).injectMembers(this);
-
-    CrawlResult result = crawler.crawl(config.getStartPages());
-    CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
-    if(config.getResultPath().isEmpty()) {
-      Writer writer = new OutputStreamWriter(System.out);
-      resultWriter.write(writer);
-      writer.flush();
-//      System.out.println(config);
-    } else {
-      Path path = Path.of(config.getResultPath());
-      resultWriter.write(path);
+    private WebCrawlerMain(CrawlerConfiguration config) {
+        this.config = Objects.requireNonNull(config);
     }
 
-    // TODO: Write the profile data to a text file (or System.out if the file name is empty)
-    if(config.getProfileOutputPath().isEmpty()) {
-      Writer writer = new OutputStreamWriter(System.out);
-      profiler.writeData(writer);
-      writer.flush();
-    } else {
-      Path path = Path.of(config.getProfileOutputPath());
-      profiler.writeData(path);
-    }
-  }
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: WebCrawlerMain [starting-url]");
+            return;
+        }
 
-  public static void main(String[] args) throws Exception {
-    if (args.length != 1) {
-      System.out.println("Usage: WebCrawlerMain [starting-url]");
-      return;
+        CrawlerConfiguration config = new ConfigurationLoader(Path.of(args[0])).load();
+        new WebCrawlerMain(config).run();
     }
 
-    CrawlerConfiguration config = new ConfigurationLoader(Path.of(args[0])).load();
-    new WebCrawlerMain(config).run();
-  }
+    private void run() throws IOException {
+        Guice.createInjector(new WebCrawlerModule(config), new ProfilerModule()).injectMembers(this);
+
+        CrawlResult result = crawler.crawl(config.getStartPages());
+        CrawlResultWriter resultWriter = new CrawlResultWriter(result);
+        // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
+        if (config.getResultPath().isEmpty()) {
+            try (Writer writer = new OutputStreamWriter(System.out)) {
+                resultWriter.write(writer);
+                writer.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            Path path = Path.of(config.getResultPath());
+            resultWriter.write(path);
+        }
+
+        // TODO: Write the profile data to a text file (or System.out if the file name is empty)
+        if (config.getProfileOutputPath().isEmpty()) {
+            try (Writer writer = new OutputStreamWriter(System.out)) {
+                profiler.writeData(writer);
+                writer.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            Path path = Path.of(config.getProfileOutputPath());
+            profiler.writeData(path);
+        }
+    }
 }
